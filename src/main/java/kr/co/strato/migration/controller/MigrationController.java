5b669023bf08a0b846e70d7d60fcadf1bb42c603
strato-ksh <strato128f@strato.co.kr>
2022-10-24 20:19:55 +0900

    백업리스트, 파드리스트, pv리스트 조회  컨트롤러 추가

---
+++ src/main/java/kr/co/strato/migration/controller/MigrationController.java
@@ -0,0 +1,62 @@
+package kr.co.strato.migration.controller;
+
+import io.fabric8.kubernetes.api.model.PersistentVolumeList;
+import io.kubernetes.client.openapi.models.V1PodList;
+import kr.co.strato.migration.model.Migration;
+import kr.co.strato.migration.model.MigrationResource;
+import kr.co.strato.migration.service.MigrationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class MigrationController {
+
+    @Autowired
+    private MigrationService migrationService;
+
+    @RequestMapping(value = "/backup/list", method = RequestMethod.GET)
+    @ResponseBody
+    public List<MigrationResource> getBackupList(@RequestParam String endpoint, @RequestParam String apiToken) {
+        Migration migration = Migration.builder()
+                .apiToken(apiToken)
+                .endpoint(endpoint)
+                .build();
+        List<MigrationResource> result = migrationService.getBackupList(migration);
+        ///System.out.println("result : "+result.toString());
+        return result;
+    }
+
+
+
+
+
+    @RequestMapping(value = "/pod/list", method = RequestMethod.GET)
+    @ResponseBody
+    public V1PodList getPodList(@RequestParam String namespace, @RequestParam String endpoint, @RequestParam String apiToken) {
+        Migration migration = Migration.builder()
+                .apiToken(apiToken)
+                .endpoint(endpoint)
+                .namespace(namespace)
+                .build();
+        V1PodList results = migrationService.getPodList(migration);
+        return results;
+    }
+
+
+
+    @RequestMapping(value = "/pv/list", method = RequestMethod.GET)
+    @ResponseBody
+    public PersistentVolumeList getPersistentVolumeList(@RequestParam String endpoint, @RequestParam String apiToken) {
+        Migration migration = Migration.builder()
+                .apiToken(apiToken)
+                .endpoint(endpoint)
+                .build();
+        PersistentVolumeList results = migrationService.getPersistentVolumeList(migration);
+        return results;
+    }
+
+
+
+}
