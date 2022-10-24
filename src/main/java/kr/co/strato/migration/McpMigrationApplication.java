a2a4867d1c4a6120f4bbf721dc95dec5cca6a38f
strato-a <strato128a@strato.co.kr>
2022-10-24 20:03:28 +0900

    servlet initializer 추가

---
+++ src/main/java/kr/co/strato/migration/McpMigrationApplication.java
@@ -0,0 +1,13 @@
+package kr.co.strato.migration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McpMigrationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(McpMigrationApplication.class, args);
+    }
+
+}
---
+++ src/main/java/kr/co/strato/migration/ServletInitializer.java
@@ -0,0 +1,14 @@
+package kr.co.strato.migration;
+
+import kr.co.strato.migration.McpMigrationApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(McpMigrationApplication.class);
+    }
+
+}
